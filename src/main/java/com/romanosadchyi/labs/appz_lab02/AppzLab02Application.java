03c7d50e96d2604539376b8fd2236a96fa39f330
romanos4740 <romanos4740@gmail.com>
2025-10-16 11:13:14 +0300

    Initial commit

---
+++ src/main/java/com/romanosadchyi/labs/appz_lab02/AppzLab02Application.java
@@ -0,0 +1,13 @@
+package com.romanosadchyi.labs.appz_lab02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppzLab02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppzLab02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/romanosadchyi/labs/appz_lab02/AppzLab02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romanosadchyi.labs.appz_lab02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppzLab02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
