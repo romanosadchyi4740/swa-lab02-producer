3722fa54eaa4d82c2b69e7d7034865dd20c19d49
romanos4740 <romanos4740@gmail.com>
2025-11-24 22:57:22 +0200

    Changed restTemplate to restClient

--- src/main/java/com/romanosadchyi/labs/appz_lab02/service/UserServiceClient.java
+++ src/main/java/com/romanosadchyi/labs/appz_lab02/service/UserServiceClient.java
@@ -1,31 +1,34 @@
 package com.romanosadchyi.labs.appz_lab02.service;
 
 import com.romanosadchyi.labs.appz_lab02.dto.UserDto;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class UserServiceClient {
-    private final RestTemplate restTemplate;
-    
+    private final RestClient restClient;
+
     @Value("${user-service.url:http://localhost:8084}")
     private String userServiceUrl;
-
-    public UserServiceClient(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
 
     public UserDto getUserById(Long id) {
         try {
             String url = userServiceUrl + "/api/users/" + id;
-            return restTemplate.getForObject(url, UserDto.class);
+
+            return restClient
+                    .get()
+                    .uri(url)
+                    .retrieve()
+                    .body(UserDto.class);
+
         } catch (Exception e) {
             log.error("Error fetching user from user-service: {}", e.getMessage());
             throw new RuntimeException("Failed to fetch user from user-service", e);
         }
     }
 }
-
